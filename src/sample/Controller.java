8dc187995cd63e259b6112ac4537637500da68f1
sotikele <xsotos@hotmail.com>
2018-06-21 21:40:44 +0300

    jar file,showTheHashFunction function,better design

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,17 +2,21 @@ package sample;
 
 
 
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.*;
@@ -31,10 +35,8 @@ public class Controller implements Initializable {
   @FXML
   private Pane pane;
 
-
-  //check the rename of repository
-
-
+  @FXML
+  private Label label,titleLabel;
 
 
 
@@ -43,7 +45,7 @@ public class Controller implements Initializable {
         table=new HashTable(15);
         textList=new ArrayList<>(15);
 
-        int squareLayoutX=0, squareLayoutY=250;
+        int squareLayoutX=-10, squareLayoutY=250;
 
         for (int i=0; i<15;i++){
 
@@ -60,8 +62,12 @@ public class Controller implements Initializable {
 
     public void insertValues(ActionEvent event){
 
+
+
        String insertValue = this.insertValue.getText();
         String insertKey = this.insertKey.getText();
+
+
        if(insertValue.equals("")&&insertKey.equals("")){
            Alert alert =new Alert(Alert.AlertType.INFORMATION);
            alert.setHeaderText("you did not give a value and a key");
@@ -88,6 +94,11 @@ public class Controller implements Initializable {
            table.put(insertKey,insertValue);
 
            textList.get(hashIndex).setText(insertKey+"\n"+insertValue);
+           this.showTheHashFunction(insertKey);
+           this.pane.getChildren().remove(label);
+
+
+
        }
 
 
@@ -156,6 +167,60 @@ public class Controller implements Initializable {
     }
 
 
+    public void showTheHashFunction(String key){
+        Label label =new Label("");
+        label.setLayoutX(-250);
+        label.setLayoutY(80);
+        label.setTextFill(Color.RED);
+        this.pane.getChildren().add(label);
+        titleLabel.setVisible(true);
+
+        int asciiNumbers=0;
+        String finalHash="";
+        for (int i=0;i<key.length();i++){
+
+            asciiNumbers+=(int) key.charAt(i);
+            label.setText(label.getText()+"ASCII value of : "+ key.charAt(i) + "    is :"+(int) key.charAt(i)+"\n");
+
+            finalHash=finalHash+(int) key.charAt(i)+"+";
+
+
+        }
+         int sizeofString=  finalHash.length();
+        finalHash=finalHash.substring(0,sizeofString-1);
+        int result=asciiNumbers %15;
+        label.setText(label.getText()+finalHash+" % 15 ="+String.valueOf(result));
+        TranslateTransition transition=new TranslateTransition();
+        transition.setDuration(Duration.seconds(2));
+        transition.setNode(label);
+        transition.setToX(500);
+
+
+
+
+
+        transition.setOnFinished(e->{
+
+            FadeTransition fadeOut=new FadeTransition(Duration.seconds(4),label);
+            fadeOut.setFromValue(1.0);
+            fadeOut.setToValue(0.0);
+            fadeOut.setOnFinished(a->{
+                titleLabel.setVisible(false);
+            });
+            fadeOut.play();
+
+
+
+
+        });
+        transition.play();
+
+
+
+
+
+
+    }
 
 
     }
